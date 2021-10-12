d19acea5de3878facf0fec101b47b4eb048429bb
suraksha shetty <surakshashetty0303@gmail.com>
2021-10-12 15:48:07 +0530

    second message

--- src/test/java/day1/NewTest.java
+++ src/test/java/day1/NewTest.java
@@ -4,7 +4,11 @@ import org.testng.annotations.Test;
 
 public class NewTest {
 @Test
-public void f() {
+public void test() {
 	System.out.println("Hello new branch");
+}
+@Test
+public void test2() {
+	System.out.println("Hello new branch 1");
 }
 }
